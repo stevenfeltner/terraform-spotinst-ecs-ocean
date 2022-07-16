f81651feebf87442a2ec16dd60674b570896b7d3
Steven Feltner <steven.feltner@netapp.com>
2022-07-16 11:51:00 -0700

    Add BDM example

--- example/block_device_mapping.tf
+++ example/block_device_mapping.tf
@@ -1,33 +1,19 @@
-terraform {
-  required_providers {
-    spotinst = {
-      source = "spotinst/spotinst"
-    }
-  }
-}
-
-provider spotinst {
-  token    = "redacted"
-  account  = "redacted"
-}
-
 #### Create Ocean ECS Cluster ####
 module "ocean_ecs" {
   source = "stevenfeltner/ecs-ocean/spotinst"
 
   cluster_name                    = "ECS-Workshop"
-  desired_capacity                = 0
-  min_size = 1
+  min_size                        = 1
   region                          = "us-west-2"
-  subnet_ids                      = ["subnet-123456789, subnet-123456789, subnet-123456789, subnet-123456789"]
-  security_group_ids              = ["sg-123456789"]
-  iam_instance_profile            = "arn:aws:iam::123456789:instance-profile/ecsInstanceRole"
+  subnet_ids                      = ["subnet-0a60fdfc059cc0c55, subnet-00dd3842d49f32f7e, subnet-013f06cd493bb19b5, subnet-041991572bdbe4edc"]
+  security_group_ids              = ["sg-000da0e1cf896a176"]
+  iam_instance_profile            = "arn:aws:iam::303703646777:instance-profile/ecsInstanceRole"
 
   block_device_mappings = {
     device_name						= "/dev/xvda"
     delete_on_termination 			= true
     encrypted 						= true
-    kms_key_id 						= "717e36d8-f059-454b-8634-123456789"
+    kms_key_id 						= "717e36d8-f059-454b-8634-48ad08b4efe6"
     snapshot_id 					= null
     iops                            = null
     volume_type 					= "gp2"
@@ -37,8 +23,4 @@ module "ocean_ecs" {
   }
 
   tags = {CreatedBy = "terraform"}
-}
-
-output "ocean_id" {
-  value = module.ocean_ecs.ocean_id
 }
